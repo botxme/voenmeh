205cbae4a44e559f3c1d88668a2a291c73f6845f
VBiralo <mr.biralo@yandex.ru>
2019-09-21 23:33:08 +0300

    выбор группы

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import { View, Panel, PanelHeader } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, Select, FormLayout } from '@vkontakte/vkui';
 import '../css/settings.css';
 
 class Settings extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      groups: ["A-141", "A-151", "A-152", "A-161", "B-151", "B-152"]
+    };
   }
 
   render() {
+    const groups = this.state.groups.map((group) => (
+      <option value={group}>{group}</option>
+    ));
+
     return (
       <Panel id="settings">
         <PanelHeader>Настройки</PanelHeader>
-        <div className="center-text">
-          А тут пусто!
-        </div>
+        <FormLayout>
+          <Select top="Группа" placeholder="Выберите группу">
+            {groups}
+          </Select>
+        </FormLayout>
       </Panel>
     );
   }
