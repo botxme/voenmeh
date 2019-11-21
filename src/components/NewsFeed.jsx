8cbb86e79ff264e4663189a65888de486fa6b05e
VBiralo <mr.biralo@yandex.ru>
2019-11-22 00:24:40 +0300

    linting NewsFeed.jsx

--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import { Panel, PanelHeader, Search, Cell, Separator, IS_PLATFORM_ANDROID, Spinner } from '@vkontakte/vkui';
+import {
+  Panel, PanelHeader, Search, Cell, Separator, IS_PLATFORM_ANDROID
+} from '@vkontakte/vkui';
 import '../css/newsfeed.css';
-import Carousel from './Carousel.jsx'
-
 import Icon24Chevron from '@vkontakte/icons/dist/24/chevron';
+import Carousel from './Carousel.jsx';
 
 class NewsFeed extends Component {
   constructor(props) {
@@ -14,7 +18,7 @@ class NewsFeed extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
-    this.FirstLetUP = this.FirstLetUP.bind(this);
+    this.firstLetterUP = this.firstLetterUP.bind(this);
   }
 
   /* поиск */
@@ -22,66 +26,72 @@ class NewsFeed extends Component {
     this.setState({ search: search.replace(/\s+/g, ' ') });
   }
 
-  FirstLetUP(str) {
-    return str[0].toUpperCase() + str.slice(1);
-  }
-
   get sposts() {
     const search = this.state.search.toLowerCase();
     const { News } = this.props;
-    if (search.substr(0, 1) == "#") {
-      return News.filter(({ tags }) => tags.join(' ').toLowerCase().indexOf(search.substr(1)) > -1)
-    } else {
-      return News.filter(({ title }) => title.toLowerCase().indexOf(search) > -1)
+    if (search.substr(0, 1) === '#') {
+      return News.filter(({ tags }) => tags.join(' ').toLowerCase().indexOf(search.substr(1)) > -1);
     }
+
+    return News.filter(({ title }) => title.toLowerCase().indexOf(search) > -1);
+  }
+
+  firstLetterUP(str) {
+    return str[0].toUpperCase() + str.slice(1);
   }
 
   render() {
-    const posts = this.sposts.length > 0 &&
-      this.sposts.map((post, id) => (
-        <div key={id} >
+    const posts = this.sposts.length > 0
+      && this.sposts.map((post) => (
+        <div key={post.id}>
           <Cell
-            key={id}
-            size='l'
-            expandable={true}
-            multiline={true}
+            size="l"
+            expandable
+            multiline
             asideContent={IS_PLATFORM_ANDROID ? <Icon24Chevron className="Cell__chevron" /> : ''}
             onClick={() => {
-              this.props.variable.goForward("page");
-              this.props.updateData.updateData(post)
+              this.props.variable.goForward('page');
+              this.props.updateData.updateData(post);
             }}
-            bottomContent={
+            bottomContent={(
               <div>
                 <div className="post_td">
                   <div className="post_tags">
                     {post.tags.map((tag, id) => (
-                      <div className="post_tag" key={id}>{this.FirstLetUP(tag)}</div>
+                      // eslint-disable-next-line react/no-array-index-key
+                      <div className="post_tag" key={id}>{this.firstLetterUP(tag)}</div>
                     ))}
                   </div>
                 </div>
                 <div className="post_bot">
                   <div className="post_date">
-                    {post.date + ' в ' + post.time}
+                    {`${post.date} в ${post.time}`}
                   </div>
                   <div className="post_author">
-                    {' · ' + post.author}
+                    {` · ${post.author}`}
                   </div>
                 </div>
               </div>
-            }
-          ><div className="post_title">{post.title}</div></Cell>
+            )}
+          >
+            <div className="post_title">{post.title}</div>
+          </Cell>
           <Separator />
         </div>
       ));
 
 
     return (
-      <Panel id="feed" >
+      <Panel id="feed">
         <PanelHeader>Новости</PanelHeader>
         <Search value={this.state.search} onChange={this.onChange} />
-        <Carousel list={this.props.banners}
-          autoplay={true}
-          sizePadding={52.63} />
+        {this.state.search.length === 0 ? (
+          <Carousel
+            list={this.props.banners}
+            autoplay
+            sizePadding={52.63}
+          />
+        ) : null}
         <div className="posts">{posts}</div>
       </Panel>
     );
