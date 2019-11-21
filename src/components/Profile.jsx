c3560ed2f5797e3923c1e9d4d68112966e4e5dca
VBiralo <mr.biralo@yandex.ru>
2019-11-21 23:53:47 +0300

    Профиль #20 и #16.4

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import { View, Panel, PanelHeader, Select, FormLayout, Div } from '@vkontakte/vkui';
+import { View, Panel, PanelHeader, Select, FormLayout, Div, Link } from '@vkontakte/vkui';
+import connect from '@vkontakte/vk-connect';
 import '../css/profile.css';
-import constants from './constants';
 
 class Profile extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      faculty: JSON.stringify(constants.api.filter(fac => {
-        return fac.faculty === localStorage.getItem('faculty');
-      })[0]),
-      group: localStorage.getItem('group')
-    };
+    this.state = {};
     this.onChange = this.onChange.bind(this);
-    this.initLocal = this.initLocal.bind(this);
   }
 
   onChange(e) {
@@ -22,50 +16,45 @@ class Profile extends Component {
     this.setState({ [name]: value });
   }
 
-  initLocal(fac) {
-    // запись расписания в localstorage для определенной группы
-    switch (fac || localStorage.getItem('faculty')) {
-      case '«И»':
-        return localStorage.setItem('shed', JSON.stringify(require('./formatGroupI.json')[JSON.parse(this.state.group).id]));
-      default:
-        return null
-    }
-  }
-
   render() {
-    this.initLocal();
-    const faculties = constants.api.map((fac, id) => (
-      <option value={JSON.stringify(fac)} key={id}>{fac.faculty}</option>
-    ))
-
-    console.log(this.state)
-
-    const groups = this.state.faculty ? JSON.parse(this.state.faculty).groups.map((fac, id) => (
-      <option value={JSON.stringify(fac)} key={id}>{fac.name}</option>
-    )) : <option value={null}></option>
 
     return (
       <Panel id="profile">
         <PanelHeader>Профиль</PanelHeader>
-        <FormLayout>
-          <Select
-            top="Факультет"
-            placeholder="Не выбран"
-            onChange={this.onChange}
-            value={this.state.faculty}
-            name="faculty">
-            {faculties}
-          </Select>
-          <Select
-            top="Группа"
-            placeholder="Не выбрана"
-            onChange={this.onChange}
-            value={this.state.group}
-            disabled={!this.state.faculty}
-            name="group">
-            {groups}
-          </Select>
-        </FormLayout>
+        <div className="profile_feedback">
+          <div className="profile_title">Обратная связь</div>
+          <div className="profile_links">
+            <Link href="https://vk.com/voenmeh_app" target="_blank">
+              <div className="profile_item">
+                <div className="profile_links_img" style={{ backgroundImage: `url(${require('../images/vk_logo.png')})` }} />
+                <div className="profile_links_text">
+                  <div className="profile_links_title">voenmeh_app</div>
+                  <div className="profile_links_sub">Сообщество сервиса</div>
+                </div>
+              </div>
+            </Link>
+
+            <Link href="https://vk.com/krethub" target="_blank">
+              <div className="profile_item">
+                <div className="profile_links_img" style={{ backgroundImage: `url(${require('../images/vk_logo.png')})` }} />
+                <div className="profile_links_text">
+                  <div className="profile_links_title">krethub</div>
+                  <div className="profile_links_sub">Владислав Кретов</div>
+                </div>
+              </div>
+            </Link>
+
+          
+              <div className="profile_item">
+                <div className="profile_links_img" style={{ backgroundImage: `url(${require('../images/globe.png')})` }} />
+                <div className="profile_links_text">
+                  <div className="profile_links_title">BOT-X.me</div>
+                  <div className="profile_links_sub">Сайт разработчика</div>
+                </div>
+              </div>
+
+          </div>
+        </div>
       </Panel>
     );
   }
