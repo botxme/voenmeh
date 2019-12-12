06b1ebaa20f080d66984cf0d66a02c6ddc013683
VBiralo <mr.biralo@yandex.ru>
2019-12-12 19:05:28 +0300

    linting Schedule.jsx + фикс #25

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,17 +1,22 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable global-require */
+/* eslint-disable array-callback-return */
+/* eslint-disable no-console */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
-import { Panel, PanelHeader, Separator, Spinner } from '@vkontakte/vkui';
+import {
+  Panel, PanelHeader, Separator
+} from '@vkontakte/vkui';
 import '../css/schedule.css';
 import DatePickerComponent from './DatePickerComponent.jsx';
-import API from '../helpers/API.js';
 
 class Schedule extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: true,
       lessons: [],
-      schedule: this.props.schedule,
-      error: null
+      schedule: this.props.schedule
     };
     this.pickDate = this.pickDate.bind(this);
   }
@@ -19,66 +24,96 @@ class Schedule extends Component {
   pickDate(d) {
     const { odd, even } = this.state.schedule;
 
-    let weekDay = d.weekday();
-    let k = d.week() / 2;
+    const weekDay = d.weekday();
+    const k = d.week() / 2;
 
-    if (weekDay == -1) return this.setState({ lessons: [null] });
+    if (weekDay === -1) return this.setState({ lessons: [null] });
     if (!odd[weekDay] || !even[weekDay]) return this.setState({ lessons: [null] });
+    if (odd[weekDay].length === 0) return this.setState({ lessons: [null] });
 
-    let les = [];
-    if (k != Math.floor(k)) {
-      console.log('четная', weekDay, k)
+    const les = [];
+    if (k !== Math.floor(k)) {
+      console.log('четная', weekDay, k);
 
-      even[weekDay].map(l => {
-        les[l.numb - 1] = l
-      })
+      even[weekDay].map((l) => {
+        les[l.numb - 1] = l;
+      });
 
-      this.setState({ lessons: les })
+      this.setState({ lessons: les });
     } else {
-      console.log('нечетная', weekDay, k)
+      console.log('нечетная', weekDay, k);
 
-      odd[weekDay].map(l => {
-        les[l.numb - 1] = l
-      })
+      odd[weekDay].map((l) => {
+        les[l.numb - 1] = l;
+      });
 
-      this.setState({ lessons: les })
+      this.setState({ lessons: les });
     }
+
+    return false;
   }
 
   render() {
     const lessons = this.state.lessons.map((les, id) => {
-      if (!les) return (
-        <div key={id} style={{
-          width: "100%",
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column'
-        }}>
-          <img src={require('../images/schedule.png')} style={{ width: "40%", marginTop: "30%" }} />
-          <span style={{ marginTop: "40px", fontWeight: "550", color: "#7f8285", width: "80%", textAlign: "center" }}>Кажется, сегодня пар нет<br />Можно отдохнуть!</span>
-        </div>
-      )
+      if (!les) {
+        return (
+          <div
+            key={id}
+            style={{
+              width: '100%',
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column'
+            }}
+          >
+            <img src={require('../images/schedule.png')} style={{ width: '40%', marginTop: '30%' }} />
+            <span style={{
+              marginTop: '40px', fontWeight: '550', color: '#7f8285', width: '80%', textAlign: 'center'
+            }}
+            >
+              Кажется, сегодня пар нет
+              <br />
+              Можно отдохнуть!
+            </span>
+          </div>
+        );
+      }
 
-      const { Time, TypeLesson, Discipline, Classroom, Lecturer, numb } = les;
-      return (<div key={id}>
-        <Separator style={{ margin: '12px 0' }} />
-        <div className="lesson">
-          <div className="lesson_time">
-            <div>{Time[0]}</div>
-            <div className="lesson_numb">{numb}</div>
-            <div>{Time[1]}</div>
-          </div>
-          <div className="lesson_border"></div>
-          <div className="lesson_content">
-            <div className="lesson_type">{TypeLesson.toUpperCase()}</div>
-            <div className="lesson_name">{Discipline}</div>
-            {Classroom ? <div className="lesson_room">Аудитория: {Classroom}</div> : false}
-            {Lecturer.ShortName ? <div className="lesson_teacher">Преподаватель: {Lecturer.FullName ? Lecturer.FullName : Lecturer.ShortName}</div> : false}
+      const {
+        Time, TypeLesson, Discipline, Classroom, Lecturer, numb
+      } = les;
+      return (
+        <div key={id}>
+          <Separator style={{ margin: '12px 0' }} />
+          <div className="lesson">
+            <div className="lesson_time">
+              <div>{Time[0]}</div>
+              <div className="lesson_numb">{numb}</div>
+              <div>{Time[1]}</div>
+            </div>
+            <div className="lesson_border" />
+            <div className="lesson_content">
+              <div className="lesson_type">{TypeLesson.toUpperCase()}</div>
+              <div className="lesson_name">{Discipline}</div>
+              {Classroom ? (
+                <div className="lesson_room">
+                  Аудитория:
+                  {' '}
+                  {Classroom}
+                </div>
+              ) : false}
+              {Lecturer.ShortName ? (
+                <div className="lesson_teacher">
+                  Преподаватель:
+                  {' '}
+                  {Lecturer.FullName ? Lecturer.FullName : Lecturer.ShortName}
+                </div>
+              ) : false}
+            </div>
           </div>
         </div>
-      </div>)
-    }
-    );
+      );
+    });
 
     return (
       <Panel id="schedule">
